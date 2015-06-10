4d78f0d6feef9adbb8d8bde7627c7645325595ec
srikanth99 <msg.9985168472@gmail.com>
2015-06-10 12:32:26 +0100

    Initial commit

---
+++ src/test/java/features/RunTest.java
@@ -0,0 +1,14 @@
+package features;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Bhaygashri on 02/06/2015.
+ */
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format ="{}")
+public class RunTest {
+}
